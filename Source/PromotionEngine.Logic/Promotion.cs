ed4b6ef1e7e73f19da4d61758690187ea7270609
Akshay Gupta <akshay.gupta@siemens-healthineers.com>
2021-06-18 23:51:18 +0530

    Added promotion logic

--- Source/PromotionEngine.Logic/Promotion.cs
+++ Source/PromotionEngine.Logic/Promotion.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace PromotionEngine.Logic
 {
     public class Promotion : IPromotion
     {
-        public Product Product { get; }
-        public int Quantity { get; }
+        public string ProductName { get; }
+        public int PromoQuantity { get; }
         public float PromotionPrice { get; }
 
-        public Promotion(Product product, int quantity, float promotionPrice)
+        public Promotion(string product, int quantity, float promotionPrice)
         {
-            Product = product;
-            Quantity = quantity;
+            ProductName = product;
+            PromoQuantity = quantity;
             PromotionPrice = promotionPrice;
         }
 
         public float ApplyPromotion(Dictionary<Product, int> products)
         {
-            throw new System.NotImplementedException();
+            var (product, quantity) = products.FirstOrDefault(p=>p.Key.Name == ProductName);
+            if (product != null)
+            {
+                int promoCount = quantity / PromoQuantity;
+                return (PromotionPrice * promoCount) + (product.Price * (quantity % PromoQuantity));
+            }
+
+            return 0;
         }
     }
 }
--- Test/PromotionEngine.Logic.Test/PromotionTest.cs
+++ Test/PromotionEngine.Logic.Test/PromotionTest.cs
@@ -10,7 +10,7 @@ namespace PromotionEngine.Logic.Test
         public void GivenPromotionWhenAppliedThenReturnsDiscountedPrice()
         {
             Promotion promotion =
-                new(new Product {Name = "A", Price = 20, IsPromotionApplied = false}, 2, 30);
+                new("A", 2, 30);
             float discountedPrice = promotion.ApplyPromotion(GetProducts());
 
             Assert.AreEqual(50, discountedPrice);
